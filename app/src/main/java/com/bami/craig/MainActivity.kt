ed0a833330759505f9ac0c6a0a829f9e9b38c644
rishb <rishbam@outlook.com>
2022-09-23 20:26:16 +0100

    Yes

--- app/src/main/java/com/bami/craig/MainActivity.kt
+++ app/src/main/java/com/bami/craig/MainActivity.kt
@@ -1,33 +1,53 @@
 package com.bami.craig
-
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
 import android.widget.ImageButton
-import android.widget.ImageView
+import android.widget.TableLayout
+import android.widget.Toolbar
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
 
-        val buttonView = findViewById<Button>(R.id.psonite)
-        buttonView.setOnClickListener{
-            val uri = Uri.parse("https://soundcloud.com/psonite")
-            val intent = Intent(Intent.ACTION_VIEW, uri)
-            startActivity(intent)
-        }
+        setSupportActionBar(findViewById(R.id.my_toolbar))
+//
+//        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-        val robsonButton = findViewById<Button>(R.id.newcastle)
-        robsonButton.setOnClickListener{
-            val uri = Uri.parse("https://nufc.co.uk/")
-            val intent = Intent(Intent.ACTION_VIEW, uri)
-            startActivity(intent)
-        }
+
     }
 
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
 
+        R.id.action_settings -> {
+            finish()
+            true
+        }
+
+        R.id.action_favorite -> {
+            true
+        }
+
+        else -> {
+            super.onOptionsItemSelected(item)
+        }
+
+    }
+
+   override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.tool_bar_items,menu)
+        return true
+    }
 }
+
+
---
+++ app/src/main/java/com/bami/craig/MenuScreen.kt
@@ -0,0 +1,11 @@
+package com.bami.craig
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MenuScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu_screen)
+    }
+}
